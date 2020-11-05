9fc92e0c7341c290c87a61a7fb1acc74f909113e
Kirill <i425496@fhict.nl>
2020-11-05 19:06:23 +0100

    Update

--- Website Project/Webfiles/NavigationBar.php
+++ Website Project/Webfiles/NavigationBar.php
@@ -7,8 +7,17 @@
 
 <nav>
 	<a href="#">Schedule</a>
+<<<<<<< HEAD
+<<<<<<< HEAD
+	<a href="#">Personal Details</a>
+=======
+	<a href="PersonalDetails.php">Personal Details</a>
+>>>>>>> Osuntuyi-Michael
+	<a href="#">Stock</a>
+=======
 	<a href="PersonalDetails.php">Personal Details</a>
 	<a href="stock.php">Stock</a>
+>>>>>>> Smirnov-Kirill
 	<?php 
 		if ($_SESSION['FormAccess'] == "StockManagerForm" || $_SESSION['FormAccess'] == "AdminForm") { echo "<a href='#'>Stock Management</a>"; } 
 	?>
@@ -18,6 +27,14 @@
 	<?php 
 		if ($_SESSION['FormAccess'] == "AdminForm") { echo "<a href='#'>Admin</a>"; } 
 	?>
+<<<<<<< HEAD
+<<<<<<< HEAD
+	<?php echo "<div> " . $_SESSION['userName'] . "</div>" ?>
+=======
 	<?php echo "<div class = 'navusername'> " . $_SESSION['userName'] . "</div>" ?>
+>>>>>>> Osuntuyi-Michael
+=======
+	<?php echo "<div class = 'navusername'> " . $_SESSION['userName'] . "</div>" ?>
+>>>>>>> Smirnov-Kirill
 	<a href="../Logic/Logout.php" class="navigationBarRight">Log out</a>
 </nav>	
