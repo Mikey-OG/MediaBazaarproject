ab27af8b156958913e1c94eee6e5ec18372650b6
1433436 <i433436@fhict.nl>
2020-11-10 23:52:32 +0100

    nav update

--- Website Project/Webfiles/NavigationBar.php
+++ Website Project/Webfiles/NavigationBar.php
@@ -16,7 +16,7 @@
 		if ($_SESSION['FormAccess'] == "EmployeeForm" || $_SESSION['FormAccess'] == "AdminForm") { echo "<a href='#'>Employee Management</a>"; } 
 	?>
 	<?php 
-		if ($_SESSION['FormAccess'] == "AdminForm") { echo "<a href='#'>Admin</a>"; } 
+		if ($_SESSION['FormAccess'] == "AdminForm") { echo "<a href='Admin.php'>Admin</a>"; } 
 	?>
 	<?php echo "<div class = 'navusername'> " . $_SESSION['userName'] . "</div>" ?>
 
