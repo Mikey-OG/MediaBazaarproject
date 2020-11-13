9d4734cab7ee6508c9cc18d3bac80f3606bef9e6
1433436 <i433436@fhict.nl>
2020-11-13 01:46:33 +0100

    Admin page stylign update

---
+++ Website Project/Webfiles/newstock.php
@@ -0,0 +1,46 @@
+<?php
+require_once ('NavigationBar.php');
+require_once('../classes/stockmodel.class.php');
+$stm = new StockModel();
+?>
+<main>
+<div class ="stockpage">
+<link rel="stylesheet" href="../Css/stockstyle.css">
+    <div class="topnav">
+        <form method="post" action="newstock.php">
+            <div class="search-container">
+            <select  data-rel="chosen"  name="categories">
+                <option>ProductID</option>
+                <option>Name</option>
+                <option>Price</option>
+                <option>Quantity</option>
+                <option>MinimumQuantity</option>
+                <option>Category</option>
+		    </select>
+                <input type="text" placeholder="Search For.." name="search">
+                <button type="submit" value="click" name="submit">Search</button> 
+            </div>
+            <div class="search-container">
+                <button type="submit" value="click" name="lowstock">Check Low Stock</button>
+            </div>
+            </form>
+    </div>
+<?php
+		if(isset($_POST['submit']))
+		{
+            $String = $_POST["categories"];
+			$Search = $_POST["search"];
+			$stm->SearchStock($Search,$String);
+        }
+        else{
+            if(isset($_POST['lowstock']))
+            {
+                $stm->ShowLowStock();
+            }	
+            else{
+                $stm->ShowStock();
+            }
+        }
+?>
+</div>
+</main>
