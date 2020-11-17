732334ed95be9644569ff66ef330bd35aed1eeda
Michael”git config --global user.name Ebowusim <447707@student.fontys.nl>
2020-11-17 21:17:50 +0100

    Update to personal details page

--- Website Project/Webfiles/Update.php
+++ Website Project/Webfiles/Update.php
@@ -4,18 +4,46 @@
 
   //$db = mysqli_select_db($conn,"dbi447707");
   mysqli_select_db($conn,'dbi435115');
+  $usr = $_SESSION['userName'];
+  
+$result = mysqli_query($conn,"SELECT * FROM employees WHERE UserName = '$usr'");
+ while($rowval = mysqli_fetch_array($result))
+ {
+
+$username= $rowval['UserName'];
+
+$fname= $rowval['FirstName'];
+
+$lname= $rowval['LastName'];
+
+$dob= date('Y-m-d', strtotime($rowval['DateOfBirth']));
+
+$email= $rowval['Email'];
+
+$phone= $rowval['PhoneNumber'];
+
+$nationality= $rowval['Nationality'];
+
+$address= $rowval['Adress'];
+
+$city= $rowval['City'];
+
+$zipcode= $rowval['ZipCode'];
+
+}
 
   if(isset($_POST['update']))
   {
     $username = $_POST['username'];
     
     $sql = "UPDATE `employees` SET FirstName = '$_POST[fname]', LastName = '$_POST[lname]', Email = '$_POST[email]', DateOfBirth = '$_POST[dob]', PhoneNumber = '$_POST[phone]', Nationality = '$_POST[nationality]',City = '$_POST[city]', ZipCode = '$_POST[zipcode]', Adress = '$_POST[address]' WHERE UserName = '$_POST[uname]'";
-
+    
     $sql_run = mysqli_query($conn, $sql);
+
 
     if($sql_run)
     {
-     header("refresh:1; url=UpdateForm.php");
+     header("refresh:1; url=PersonalDetails.php");
      echo '<script type="text/javascript"> alert("Your details have been updated")</script>';
     }
     else
