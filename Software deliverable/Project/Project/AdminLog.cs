89e9ee39c5faf09a35e70ddb8460371342035b42
1433436 <i433436@fhict.nl>
2021-01-14 14:41:01 +0100

    Add new solution

---
+++ Software deliverable/Project/Project/AdminLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Project.DAL;
+
+namespace Project
+{
+    public partial class AdminLog : Form
+    {
+        EmployeeManagementDAL gm;
+
+        public AdminLog()
+        {
+            InitializeComponent();        
+            gm = new EmployeeManagementDAL();
+        }
+
+        private void btnViewLogs_Click(object sender, EventArgs e)
+        {
+            gm.FillWithLogs(ltbAdminLog);
+        }
+
+        private void btnDeleteLog_Click(object sender, EventArgs e)
+        {
+            //gm.RemoveLog();
+        }
+    }
+}
