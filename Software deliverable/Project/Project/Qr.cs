6130c479f980e7d9082360b84add098c49d7823d
1433436 <i433436@fhict.nl>
2021-01-16 15:04:11 +0100

    Software Solution added

---
+++ Software deliverable/Project/Project/Qr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using Project.LGC;
+
+
+namespace Project
+{
+    public partial class Qr : Form
+    {
+        private Encryption encry = new Encryption();
+        public Qr()
+        {
+            InitializeComponent();
+        }
+
+        private void Qr_Load(object sender, EventArgs e)
+        {
+            string code = "http://localhost/Website%20Project/Webfiles/Attendance.php?Key=" + encry.Encrypt(DateTime.Now.ToString("dd-MM-yyyy") + Variables.User);
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(8);
+            this.pictureBox1.Image = qrCodeImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://localhost/Website%20Project/Webfiles/Attendance.php?Key=" + encry.Encrypt(DateTime.Now.ToString("dd-MM-yyyy") + Variables.User));
+        }
+
+
+    }
+}
