17f2b328714ba2294e243d4f4c54c97814062348
1433436 <i433436@fhict.nl>
2021-01-17 05:42:15 +0100

    New website

---
+++ Website Project/DataBase/GetUserInfo.php
@@ -0,0 +1,38 @@
+<?php 
+
+	require_once ('Connection.php');
+
+    class UserInfo {
+
+        public function getUserInlog(string $userName, int $i): string {
+
+            $result = array();
+
+            $Connection = new Connection();
+
+            $sql = "SELECT employee.UserName, employee.Password, roles.FormAccess, employee.UserID FROM employee INNER JOIN roles ON employee.RoleID = roles.RoleID WHERE employee.UserName = ?";
+            $stmt = $Connection->connect()->prepare($sql);
+
+            $stmt->execute([$userName]);
+            $pdoResult = $stmt->fetchAll();
+            
+        foreach($pdoResult as $record) {
+            $result[0] = $record['UserName'];
+            $result[1] = $record['Password'];
+            $result[2] = $record['FormAccess'];
+            $result[3] = $record['UserID'];
+        }
+
+        if($result == null) {
+            $result[0] = "null";
+            $result[1] = "null";
+            $result[2] = "null";
+            $result[3] = "null";    
+        }
+
+        return $result[$i];
+
+        }
+
+    }
+?>
